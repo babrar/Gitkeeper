df4928533b82c97e823e7f374f9d5202abd82477
babrar <baninabrar98@gmail.com>
2018-07-16 01:25:23 -0400

    Log dir added. Minor fixes

--- setup.sh
+++ setup.sh
@@ -1,6 +1,18 @@
 #!/bin/bash 
 
+
 # dirname removes suffix starting with '/' from PWD
 # get path one level up dir
 parentdir="$(dirname "$(pwd)")"
+
+# add Gitook/ and initrepo.sh to user's .gitignore
+# if Gitook is run for the first time
+if [ ! -f $parentdir/initrepo.sh ]; then
+	echo "Gitook/" >> $parentdir/.gitignore
+	echo "initrepo.sh" >> $parentdir/.gitignore
+fi
+
+# move initrepo.sh to repo root
 cp initrepo.sh $parentdir
+
+
